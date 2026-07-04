011f5657ec72c3039f2bdddbaf3d51ce9ea1caa6
Velune <69484858+Velune-VT@users.noreply.github.com>
2026-07-04 19:14:19 -0400

    Delete forms.js

--- forms.js
+++
@@ -1,7 +0,0 @@
-window.FORMS = [
-  { id:'horse', name:'Horse', icon:'🐴', rarity:'✦', usesSkin:false },
-  { id:'centaur', name:'Centaur', icon:'🎠', rarity:'✦', usesSkin:true },
-  { id:'hippocampus', name:'Hippocampus', icon:'🌊', rarity:'✦✦', usesSkin:false },
-  { id:'satyr', name:'Satyr', icon:'🐐', rarity:'✦✦', usesSkin:true },
-  { id:'hippocampustaur', name:'Hippocampustaur', icon:'🫧', rarity:'✦✦✦', usesSkin:true }
-];
